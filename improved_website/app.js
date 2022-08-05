ea90082e10dc9328612ef735f54c3e5a6e6c7086
Luke Powell <luke.ar.powell@btinternet.com>
2022-08-05 21:19:06 +0100

    Simple Website

---
+++ improved_website/app.js
@@ -0,0 +1,17 @@
+"use strict";
+// Reference the button on html page
+const switcher = document.querySelector('.btn');
+// Event Handler for clicking button
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if (className == "light-theme"){
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    console.log("current class name: " + className);
+});
+
